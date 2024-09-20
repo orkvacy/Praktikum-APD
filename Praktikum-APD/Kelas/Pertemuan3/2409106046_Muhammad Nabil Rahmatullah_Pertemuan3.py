b501988fa0d7c49159e3ba7a7d55dd9476aa262f
orkvacy <krotago@gmail.com>
2024-09-20 14:56:34 +0800

    Add files via upload

---
+++ Praktikum-APD/Kelas/Pertemuan3/2409106046_Muhammad Nabil Rahmatullah_Pertemuan3.py
@@ -0,0 +1,70 @@
+# # # # # # # if cuaca == "cerah":
+# # # # # # #     print("kamu pergi keluar rumah")
+
+# # # # # # # # variabel akan bernilai false jika string kosong, ,0 ,[], {}, ()
+# # # # # # # uang = 1
+# # # # # # # if uang:
+# # # # # # #     print("Pergi ke Pasar!")
+# # # # # # # else
+# # # # # # #     print("Pergi ke rumah teman")
+
+
+# # # # # # nilai = 10
+# # # # # # if nilai < 0:
+# # # # # #     print("bilangan negatif")
+# # # # # # else
+# # # # # #     print("bilangan positif")
+
+# # # # # umur = int(input("masukkan umur"))
+# # # # # if umur <= 10:
+# # # # #     print("Umur anda masuk kategori anak-anak")
+# # # # # elif umur<=18:
+# # # # #     print("Umur anda masuk kategori remaja")s
+# # # # # elif umur <=35:
+# # # # #     print("Umur anda masuk kategori Dewasa")
+# # # # # elif umur <=65:
+# # # # #     print("Umur anda masuk kategori paruh baya")
+# # # # # else:
+# # # # #     print("Umur anda termasuk kategori Tua")
+
+# # # # jenisKelamin = input("Jenis Kelamin (L/P)").upper()
+# # # # hasil = "Laki-Laki" if jenisKelamin == "L" else "perempuan"
+# # # # print(f"Jenis kelamin {hasil}")
+
+# # # nilai = 76
+# # # if nilai >= 80:
+# # #     if nilai >=85 :
+# # #         print("nilai A+")
+# # #     else :
+# # #         print("Nilai A-")
+# # # else :
+# # #     print("Tidak Lulus")
+
+# # print("Menu:")
+# # print("1. Ngoding")
+# # print("2. Ngoding")
+# # print("3. Keluar")
+
+# # hasil = int(input("masukkan pilihan : "))
+# # print("==============")
+# # if hasil==1:
+# #     print("sedang ngoding")
+# # elif hasil==2:
+# #     print("sedang ngoding")
+# # elif hasil==3:
+# #     print("sedang keluar")
+# # else:
+# #     print("tidak ada dimenu")
+# # print("==============")
+
+# #perkondisian
+angka = int(input("masukkan angka : "))
+# if angka % 2 ==0:
+#     print("angka adalah genap")
+# if angka >0:
+#     print("angka adalah positif")
+
+if angka % 2 == 0:
+    print("angka adalah genap")
+if angka > 0:
+    print("positif")
