d087069cfe137ac346a0aac3b16055bc7d444273
orkvacy <krotago@gmail.com>
2024-09-27 15:00:39 +0800

    Add files via upload

---
+++ Praktikum-APD/Kelas/Pertemuan4/2409106046_MuhammadNabilRahmatullah_Pertemuan4.py
@@ -0,0 +1,61 @@
+# # # # # # # # batas = 10
+# # # # # # # # for i in range(batas):
+# # # # # # # #     print(f"Perulangan ke-{i}")
+
+# # # # # # # buahBanyak = ["Apel", "Mangga", "Anggur"]
+# # # # # # # for buah in buah_banyak:
+# # # # # # #     print(buah)
+
+# # # # # # for baris in range(1, 4):
+# # # # # #     print("Baris ke", baris)
+# # # # # #     for kolom in range(1, 5):
+# # # # # #         print(kolom, "kolom", end=" ")
+# # # # # #     print()
+
+# # # # # #     print()
+
+# # # # # lagi = "yes"
+# # # # # total = 0
+# # # # # while lagi == "yes":
+# # # # #     print("Mabar")
+# # # # #     lagi = input("Mabar lagi ga ")
+# # # # #     total += 1
+# # # # # print("Selesai Mabar")
+
+# # # # # print(f"Perulangan terjadi sebanyak {total}")
+
+# # # # for i in range(1,10):
+# # # #     if i == 4:
+# # # #         break
+# # # #     else:
+# # # #         print(f"perulangan ke {i}")
+
+# # # while True:
+# # #     ulang = input("Mabar Lagi")
+# # #     if ulang == "ga":
+# # #         break
+# # #     print("Mabar lagi")
+
+# # for i in range(1,10):
+# #     if i == 4:
+# #         continue
+# #     print(f"Angka ke-{i}")
+
+# # n = 5  
+# # for i in range(1, n + 1):
+# #     print("*" * i)
+
+# kesempatan = 3
+
+# while kesempatan > 0:
+#     username = input("masukkan username : ")
+#     password = input("masukkan password : ")
+#     if username == "admin" and password == "admin123":
+#         print("Berhasil Login")
+#         break
+#     else:
+#         print("Username atau password salah")
+#         kesempatan -= 1
+#         print(f"Kesempatan login tersisa {kesempatan} kali")
+
+# print("Main Menu")
