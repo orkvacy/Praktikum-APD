b10d93e36460351583cdd45f193af312250fd67c
orkvacy <krotago@gmail.com>
2024-10-04 15:03:13 +0800

    Add files via upload

---
+++ Praktikum-APD/Kelas/Pertemuan5/2409106046_MuhammadNabilRahmatullah_Pertemuan5.py
@@ -0,0 +1,47 @@
+# # # # # # tas = ["buku", 32, True, 3.14, ["IF", 24]]
+# # # # # # print(tas[4])
+
+# # # # # data = ["apel", "semangka", "anggur"]
+# # # # # # print(data)
+# # # # # data.append("rambutan")
+# # # # # print(data)
+# # # # # data.insert(0, "mangga")
+# # # # # print (len(data))
+# # # # # data.extend(["kelengkeng", "kiwi"])
+# # # # # print(data)
+
+# # # # data = ["apel", "semangka", "anggur"]
+# # # # for i in data:
+# # # #     print(i)
+
+# # # # # data.pop(1)
+# # # # # print(data)
+# # # # # del data[0]
+# # # # # print(data)
+
+# # # # # data[0] = "Anggur"
+# # # # # print(data)
+
+# # # dataMahasiswa = [
+# # #     ["046", "Nabil","APD", "ORSIKOM"],
+# # #     ["050", "tes"]
+# # # ]
+
+# # # print(dataMahasiswa[0][1])
+
+# # data = ("apel", "semangka", "anggur", "apel")
+# # index = [i for i, x in enumerate(data) if x == "apel"]
+# # for tes in index:
+# #     print(tes)
+
+# dataMahasiswa = ["Nabil", "tes", "tes1", "tes2"]
+# print(dataMahasiswa)
+# # tambah = input("apa yang mau ditambah")
+# # dataMahasiswa.append(tambah)
+# # print(dataMahasiswa)
+
+# # hapus = int(input("data ke berapa yang mau dihapus"))
+# # dataMahasiswa.pop(hapus)
+# # print(dataMahasiswa)
+
+
