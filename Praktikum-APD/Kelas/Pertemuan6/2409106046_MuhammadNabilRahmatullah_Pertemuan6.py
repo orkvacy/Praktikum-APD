3f7c6b3547da560750b6933ae9561a3512dacdac
orkvacy <krotago@gmail.com>
2024-10-11 14:48:45 +0800

    Add files via upload

---
+++ Praktikum-APD/Kelas/Pertemuan6/2409106046_MuhammadNabilRahmatullah_Pertemuan6.py
@@ -0,0 +1,60 @@
+# dataMahasiswa = {
+#     "nama" : "ucup",
+#     "nim" : "021"
+# }
+
+# for keys in dataMahasiswa:
+#     print(keys)
+
+# for value in dataMahasiswa.values():
+#     print(value)
+
+
+# keyBaru = input("masukkan Key nya : ")  #nambah
+# valueBaru = input("masukkan Value nya : ")
+
+# dataMahasiswa[keyBaru] = valueBaru
+# print(dataMahasiswa)
+
+# keyUbah = input("masukkan Key nya : ")      #ubah
+# valueUbah = input("masukkan Value nya : ")
+
+# dataMahasiswa[keyUbah] = valueUbah
+# print(dataMahasiswa)
+
+# hapusData = input("pilih key nya : ")   #hapus
+# dataMahasiswa.pop(hapusData)
+# print(dataMahasiswa)
+# print(dataMahasiswa, [hapusData])
+
+
+# # key = "apel", "jeruk", "mangga"
+# # value = 1
+# # buah = dict.fromkeys(key, value)
+# # print(buah)
+
+
+
+# # for data in dataMahasiswa:
+# #     print(data)
+
+# # for data, nilai in dataMahasiswa.items():
+# #     print(f"Nilai {data}, anda  yaitu {nilai}")
+# # # print(dataMahasiswa.get('matkul', 'gada'))
+
+# # # print(dataMahasiswa["nama"])
+# # # print(dataMahasiswa["nim"])
+
+# # dataMahasiswa = [
+# #     {
+# #     'nama' : 'ucup',
+# #     "role" : "admin"
+# #     },
+
+# #     {
+# #     'nama' : "budi",
+# #     'role' : 'user'
+# #     }
+# # ]
+
+# # print(dataMahasiswa[0]['nama'])
